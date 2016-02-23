1ee501d7760b689fa05c718bd1f34a31c603efa6
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-02-23 15:24:18 +0200

    Change name file

--- sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
+++ sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
@@ -1,9 +1,8 @@
 package inna.qa.dp;
 
 public class MyFirstProgramm {
-	
-	public static void main(String[] args){
-		
-		System.out.println("Hello, world");
-	}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
 }
