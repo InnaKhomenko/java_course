92defc6f10c3332b3a33c54e8f26e0f77f8d1463
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-02-25 22:40:10 +0200

    Правки ДЗ

--- sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
+++ sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
@@ -2,12 +2,12 @@ package inna.qa.dp;
 
 public class MyFirstProgramm {
 
-    public static double distance(Point p1, Point p2) {
+    public static double distance(Point p1, Point p2) { //функция, вычисляющая расстояние между двумя точками
         return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {//запускаемый класс
+//объявляем координаты (x,y) 2х точек
         double x1 = 4;
         double x2 = 3;
         double y1 = 3;
@@ -15,8 +15,9 @@ public class MyFirstProgramm {
 
         Point p1 = new Point(x1, y1);
         Point p2 = new Point(x2, y2);
-        System.out.println("Расстояние между точками " + " = " + (distance(p1, p2)));
-        System.out.println("Расстояние между точками " + " = " + (p1.distance2(p1, p2)));
+
+        System.out.println("Расстояние между точками " + " = " + (distance(p1, p2)));//вызов функции вычисления расстояния
+        System.out.println("Расстояние между точками " + " = " + (p1.distance2(p1, p2)));//вызов метода вычисления расстояния в классе Point
 
     }
 
--- sandbox/src/main/java/inna/qa/dp/Point.java
+++ sandbox/src/main/java/inna/qa/dp/Point.java
@@ -1,6 +1,6 @@
 package inna.qa.dp;
 
-public class Point {
+public class Point {//создание класса Point
 
     public double x;
     public double y;
@@ -10,7 +10,7 @@ public class Point {
         this.y = y;
     }
 
-    public double distance2(Point p1, Point p2) {
+    public double distance2(Point p1, Point p2) {//метод вычисления расстояния в классе Point
         return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
     }
 
