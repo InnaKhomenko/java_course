50442c6cc499aa6ae1aa6f52206cb29ffd660af6
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-02-25 20:50:12 +0200

    add file

--- sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
+++ sandbox/src/main/java/inna/qa/dp/MyFirstProgramm.java
@@ -1,8 +1,18 @@
 package inna.qa.dp;
 
+
 public class MyFirstProgramm {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+
+    double x1 = 4;
+    double x2 = 3;
+    double y1 = 3;
+    double y2 = 6;
+
+    Point pp1 = new Point(x1, y1, x2, y2);
+
+    System.out.println("Расстояние между точками " + " = " + (pp1.distance(pp1,pp1,pp1,pp1)));
+
   }
 }
