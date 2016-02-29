d0d89bb44431ed223dc4298d5cd2ace465c58ea8
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-02-29 17:26:59 +0200

    первый тест

--- sandbox/src/main/java/inna/qa/dp/Point.java
+++ sandbox/src/main/java/inna/qa/dp/Point.java
@@ -2,16 +2,23 @@ package inna.qa.dp;
 
 public class Point {//создание класса Point
 
-  public double x;
-  public double y;
+    public double x;
+    public double y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  public double distance2(Point p1, Point p2) {//метод вычисления расстояния в классе Point
-    return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
-  }
+    public double distance_P1(Point p1, Point p2) {
+        return Math.pow(p2.x - p1.x, 2);
+    }
 
-}
+    public double distance_P2(Point p1, Point p2) {
+        return Math.pow(p2.y - p1.y, 2);
+    }
+
+    public double distance2(Point p1, Point p2) {//метод вычисления расстояния в классе Point
+        return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
+    }
+ }
---
+++ sandbox/src/test/java/inna/qa/dp/PointTests.java
@@ -0,0 +1,22 @@
+package inna.qa.dp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+    @Test
+    public void TestDistance() {
+
+        double x1 = 4;
+        double x2 = 3;
+        double y1 = 3;
+        double y2 = 6;
+
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+
+        Assert.assertEquals(p2.distance2(p1, p2), 3.1622776601683795);
+        Assert.assertEquals(p1.distance_P1(p1, p2), 1.0);
+        Assert.assertEquals(p1.distance_P2(p1, p2), 9.0);
+    }
+}
