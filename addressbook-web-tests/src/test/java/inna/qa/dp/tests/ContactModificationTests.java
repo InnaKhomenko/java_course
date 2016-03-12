8153e0512e72156ec471a46886ef25e6a6ef1841
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-03-13 00:03:43 +0300

    Запуск тестов с разных браузеров

--- addressbook-web-tests/src/test/java/inna/qa/dp/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/inna/qa/dp/tests/ContactModificationTests.java
@@ -7,10 +7,8 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getContactHelper().goToContactsPage();
-        //app.getContactHelper().selectContact();
         app.getContactHelper().editContact();
         app.getContactHelper().addInfoContact(new groupInfoContact("test11may", "inna23 ", "khomenko", "mio", "ddd", "zp", "ukraine", "www", "323434545454545", "56565656565656", "56565656565656", "fdff@rr.rrr", "rtttrtr@rrr.rttr", "333@rr.rtrt", "trrtrtrt", "545454545", "rrttttyty"));
-       // app.getContactHelper().submitContactModification();
         app.getContactHelper().updateContact();
         app.getContactHelper().goToContactsPage();
     }
---
+++ sandbox/src/main/java/inna/qa/dp/Equality.java
@@ -0,0 +1,11 @@
+package inna.qa.dp;
+
+/**
+ * Created by Inna on 12.03.2016.
+ */
+public class Equality {
+
+    public static void main(String[] args){
+
+    }
+}
