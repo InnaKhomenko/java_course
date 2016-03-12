16de8533f34f36bcbc93d9c98ea437798bb60ec5
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-03-13 02:37:07 +0300

    Модификация

--- addressbook-web-tests/src/test/java/inna/qa/dp/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/inna/qa/dp/tests/GroupModificationTests.java
@@ -7,6 +7,10 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroupe(new GroupData("test1", null, null));
+        }
+        app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().feelGroupForm(new GroupData("test1", null, null));
