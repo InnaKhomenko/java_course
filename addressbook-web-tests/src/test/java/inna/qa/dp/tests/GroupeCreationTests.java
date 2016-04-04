9f9e2611e6448989dac18e692568d4021d91ab7e
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-04-04 16:49:38 +0300

    Улучшение проверок (создание групп)

--- addressbook-web-tests/src/test/java/inna/qa/dp/tests/GroupeCreationTests.java
+++ addressbook-web-tests/src/test/java/inna/qa/dp/tests/GroupeCreationTests.java
@@ -1,12 +1,10 @@
 package inna.qa.dp.tests;
 
 import inna.qa.dp.model.GroupData;
-import org.testng.Assert;
+import inna.qa.dp.model.Groups;
 import org.testng.annotations.Test;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupeCreationTests extends TestBase {
@@ -14,15 +12,13 @@ public class GroupeCreationTests extends TestBase {
     @Test
     public void testGroupeCreation() {
         app.goTo().groupPage();
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData group = new GroupData().withtName("test3");
         app.group().create(group);
         app.goTo().groupPage();
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
-
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-        before.add(group);
-        Assert.assertEquals(before, after);
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
