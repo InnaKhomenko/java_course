43f171580fd955abd8236176e460d39a2c67ee52
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-04-11 18:02:08 +0300

    Загрузка тестовых данных из файла (контакты)

--- addressbook-web-tests/src/test/java/inna/qa/dp/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/inna/qa/dp/tests/ContactCreationTests.java
@@ -2,10 +2,14 @@ package inna.qa.dp.tests;
 
 import inna.qa.dp.model.ContactData;
 import inna.qa.dp.model.Contacts;
+import inna.qa.dp.model.GroupData;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,21 +20,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts(){
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src/test/resources/zacat.jpg");
-        list.add(new Object[] {new ContactData().withAddress("ukraine").withCompany("test3").withEmail1("inna@dd.com")
-                .withEmail2("333@rr.rtrt").withEmail3("rtttrtr@rrr.rttr").withFax("trrtrtrt").withFirstname("inna23")
-                .withPhoto(photo).withHome("4455").withLastname("khomenko").withMobile("56565656565656").withName("zp")
-                .withFax("56565656565656").withWork("323434545454545")});
-        list.add(new Object[] {new ContactData().withAddress("ukraine").withCompany("test3").withEmail1("inna@dd.com")
-                .withEmail2("333@rr.rtrt").withEmail3("rtttrtr@rrr.rttr").withFax("trrtrtrt").withFirstname("inna23")
-                .withPhoto(photo).withHome("4422").withLastname("khomenko").withMobile("56565656565656").withName("zp")
-                .withFax("56565656565656").withWork("323434545454545")});
-        list.add(new Object[] {new ContactData().withAddress("ukraine").withCompany("test3").withEmail1("inna@dd.com")
-                .withEmail2("333@rr.rtrt").withEmail3("rtttrtr@rrr.rttr").withFax("trrtrtrt").withFirstname("inna23")
-                .withPhoto(photo).withHome("44225").withLastname("khomenko").withMobile("56565656565656").withName("zp")
-                .withFax("56565656565656").withWork("323434545454545")});
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.csv"));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withAddress(split[0]).withCompany(split[1]).withEmail1(split[2])
+                    .withEmail2(split[3]).withEmail3(split[4]).withFax(split[5]).withFirstname(split[6])
+                    .withPhoto(photo).withHome(split[7]).withLastname(split[8]).withMobile(split[9]).withName(split[10])
+                    .withFax(split[11]).withWork(split[12])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
