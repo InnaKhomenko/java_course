cd6edc1dc6f4cb5a00442102111e432524e54b61
InnaKhomenko <inna.khomenko.test@gmail.com>
2016-04-13 17:27:49 +0300

    Сохранение файлов в формате json

--- addressbook-web-tests/src/test/java/inna/qa/dp/model/ContactData.java
+++ addressbook-web-tests/src/test/java/inna/qa/dp/model/ContactData.java
@@ -1,32 +1,58 @@
 package inna.qa.dp.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 import java.util.SplittableRandom;
 
 public class ContactData {
+    @Expose
     private String firstname;
+    @Expose
     private String name;
+    @Expose
     private String lastname;
+    @Expose
     private String nickname;
+    @Expose
     private String title;
+    @Expose
     private String company;
+    @Expose
     private String address;
+    @Expose
     private String home;
+    @Expose
     private String mobile;
+    @Expose
     private String work;
+    @Expose
     private String fax;
+    @Expose
     private String phone2;
+    @Expose
     private String email1;
+    @Expose
     private String email2;
+    @Expose
     private String email3;
+    @Expose
     private String notes;
+    @Expose
     private String group;
+    @Expose
     private String middlename;
+    @Expose
     private String allNames;
+    @Expose
     private String allPhones;
+    @Expose
     private String allAddress;
+    @Expose
     private String allEmails;
+    @Expose
     private String allContent;
+    @Expose
     private File photo;
 
     public ContactData withPhoto(File photo) {
